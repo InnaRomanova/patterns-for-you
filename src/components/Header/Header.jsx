fef9dd96fa4819ce4cfac3e2cce45748468f1ae0
unknown <romanovainnap@yandex.ru>
2023-10-29 11:46:09 +0300

    header descktop

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.css";
 import Logo from "../../images/Logo.svg";
+import Seach from "../../images/search.svg";
+import SignInUp from "../../images/signInUp.svg";
 
 function Header() {
   return (
@@ -12,7 +14,11 @@ function Header() {
         <div className="header__menu">
           <ul className="header__lists">
             <li className="header__link">
-              <a className="header__link-item" href="#" target="_blank">
+              <a
+                className="header__link-item header__link-item_active"
+                href="#"
+                target="_blank"
+              >
                 Главная
               </a>
             </li>
@@ -38,9 +44,22 @@ function Header() {
             </li>
           </ul>
         </div>
-        <div className="header__sign"></div>
+        <div className="header__seach">
+          <img className="header__seach-image" src={Seach} alt="поиск" />
+        </div>
+        <div className="header__sign">
+          <img
+            className="header__sign-image"
+            src={SignInUp}
+            alt="авторизация/регистрация"
+          />
+          <button className="header__sign-button header__sign-in">Вход</button>
+          <span className="header__slesh">/</span>
+          <button className="header__sign-button header__sign-up">
+            Регистрация
+          </button>
+        </div>
       </div>
-      Привет
     </header>
   );
 }

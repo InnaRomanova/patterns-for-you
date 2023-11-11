fcec292cc3fd1800cc3277d538ab9addffe8d3be
unknown <romanovainnap@yandex.ru>
2023-11-11 12:12:02 +0300

    News desctop in cards

---
+++ src/components/Elements/ButtonVector.jsx
@@ -0,0 +1,20 @@
+export default function ButtonVectorLeft() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="8"
+      height="15"
+      viewBox="0 0 8 15"
+      fill="none"
+    >
+      <path d="M6.94286 0.999755L1 7.43141L6.94286 13.8631" fill="white" />
+      <path
+        d="M6.94286 0.999755L1 7.43141L6.94286 13.8631"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./News.css";
 import NewsCard from "../../images/NewsCard.png";
+import ButtonVector from "../Elements/ButtonVector";
+import ButtonVectorLeft from "../Elements/ButtonVector";
+import ButtonVectorLeftRight from "../Elements/ButtonVectorRight";
 
 function News() {
   return (
@@ -10,25 +13,86 @@ function News() {
           <h2 className="news__title">Новинки!</h2>
           <p className="news__subtitle">Новые модели выкроек!</p>
         </div>
-        <div className="news__cards">
-          <div className="news__card">
-            <img
-              src={NewsCard}
-              alt="картинка выкройки"
-              className="card__image"
-            />
-            <div className="card__inform">
-              <h3 className="card__name">Modern Kitchen</h3>
-              <p className="card__katalog">женское платье</p>
-              <span className="card__price">
-                135.00 <p>Руб.</p>
-              </span>
-            </div>
-            <div className="card__more">
-              <p className="card__katalog">Подробнее</p>
-              <button className="card__button-katalog"></button>
+        <div className="news__cards-list">
+          <div className="news__cards">
+            <div className="news__card">
+              <img
+                src={NewsCard}
+                alt="картинка выкройки"
+                className="card__image"
+              />
+              <div className="card__inform">
+                <h3 className="card__inform-name">Modern Kitchen</h3>
+                <p className="card__inform-katalog">женское платье</p>
+                <span className="card__inform-price">135.00 Руб.</span>
+              </div>
+              <div className="card__more">
+                <p className="card__more-katalog">Подробнее</p>
+                <button className="card__more-button-katalog"></button>
+              </div>
             </div>
           </div>
+          <div className="news__cards">
+            <div className="news__card">
+              <img
+                src={NewsCard}
+                alt="картинка выкройки"
+                className="card__image"
+              />
+              <div className="card__inform">
+                <h3 className="card__inform-name">Modern Kitchen</h3>
+                <p className="card__inform-katalog">женское платье</p>
+                <span className="card__inform-price">135.00 Руб.</span>
+              </div>
+              <div className="card__more">
+                <p className="card__more-katalog">Подробнее</p>
+                <button className="card__more-button-katalog"></button>
+              </div>
+            </div>
+          </div>
+          <div className="news__cards">
+            <div className="news__card">
+              <img
+                src={NewsCard}
+                alt="картинка выкройки"
+                className="card__image"
+              />
+              <div className="card__inform">
+                <h3 className="card__inform-name">Modern Kitchen</h3>
+                <p className="card__inform-katalog">женское платье</p>
+                <span className="card__inform-price">135.00 Руб.</span>
+              </div>
+              <div className="card__more">
+                <p className="card__more-katalog">Подробнее</p>
+                <button className="card__more-button-katalog"></button>
+              </div>
+            </div>
+          </div>
+          <div className="news__cards">
+            <div className="news__card">
+              <img
+                src={NewsCard}
+                alt="картинка выкройки"
+                className="card__image"
+              />
+              <div className="card__inform">
+                <h3 className="card__inform-name">Modern Kitchen</h3>
+                <p className="card__inform-katalog">женское платье</p>
+                <span className="card__inform-price">135.00 Руб.</span>
+              </div>
+              <div className="card__more">
+                <p className="card__more-katalog">Подробнее</p>
+                <button className="card__more-button-katalog"></button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="news__button">
+          <button className="news__button-vector news__button-left">
+            <ButtonVectorLeft />
+          </button>
+          <ButtonVectorLeftRight />
+          {/* <button className="news__button-vector news__button-right"></button> */}
         </div>
       </div>
     </section>

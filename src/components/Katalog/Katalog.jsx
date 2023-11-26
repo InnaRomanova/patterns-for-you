a46a123351baf86931382b472826c093e3614c31
unknown <romanovainnap@yandex.ru>
2023-11-26 19:06:33 +0300

    component Katalog is desktop

--- src/components/Katalog/Katalog.jsx
+++ src/components/Katalog/Katalog.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Katalog.css";
 import CardButton from "../Elements/CardButton";
-import katalogImage1 from "../../images/katalogImage1.png";
+// import katalogImage1 from "../../images/katalogImage1.png";
 
 function Katalog() {
   return (
@@ -13,11 +13,26 @@ function Katalog() {
         </div>
         <div className="katalog-cards">
           <div className="katalog-card">
-            <img
+            {/* <img
               src={katalogImage1}
               alt="фото выкройки"
               className="katalog-card__image"
-            />
+            /> */}
+            <div className="katalog-card__info">
+              <div className="katalog-card__info-text">
+                <h4 className="katalog-card__name">Платья</h4>
+                <p className="katalog-card__detailed">Decor / Подробнее</p>
+              </div>
+              <CardButton />
+            </div>
+          </div>
+
+          <div className="katalog-card">
+            {/* <img
+              src={katalogImage1}
+              alt="фото выкройки"
+              className="katalog-card__image"
+            /> */}
             <div className="katalog-card__info">
               <div className="katalog-card__info-text">
                 <h4 className="katalog-card__name">Платья</h4>

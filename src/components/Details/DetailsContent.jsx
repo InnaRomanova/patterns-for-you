f81e49ffeea197b3fd0618d7eb6ddc3aa8fbf9ec
InnaRomanova <RomanovaInnaP@yandex.ru>
2024-04-25 12:28:05 +0300

    Details and DetailsContent is openList text

--- src/components/Details/DetailsContent.jsx
+++ src/components/Details/DetailsContent.jsx
@@ -9,6 +9,7 @@ import DetailsModel from "../Details/DetailsModel";
 import { arrClothes } from "../Constants/Objects/Massiv";
 import map from "lodash/map";
 import DetailsKategories from "./DelailsKategories";
+import Navigation from "../Navigation/Navigation";
 // import Like from "../Elements/Like";
 // import Comment from "../Elements/Comment";
 
@@ -29,7 +30,6 @@ function DetailsContent({ cardValuePictures, cardValue }) {
 
     return (
         <>
-
             <div className="details__content">
                 {mobile ? (<>
                     <DetailsModel cardValuePictures={cardValuePictures} />
@@ -44,85 +44,84 @@ function DetailsContent({ cardValuePictures, cardValue }) {
 
                             <li className="details__sidmaster-item">
                                 <ul className="details__sidebar-list">
-                                    {openList ? (
-                                        <li className = "details__sidebar-item" onClick={handleClose}>
-                                        Расход материала b n/l/l/l/l
-                                        <DetailsVectorSidbar/></li>
-                                    ): (
-                                            <li className = "details__sidebar-item">
+                                    {openList ? (<li className="details__sidebar-item details__sidebar-item_active" onClick={handleClose}>
+                                        gggggggggggggggggggggggggg
+                                        </li>) : (
+                                        <li className="details__sidebar-item">
+                                            Расход материала
+                                            <DetailsVectorSidbar onClick={handleOpen} /></li>)}
+
+                                    {/* {openList ? (<Navigation handleClose={handleClose} />) : ""} */}
+
+                                    <li className="details__sidebar-item">
+                                        Рекомендации по материалам
+                                        <DetailsVectorSidbar /></li>
+                                    <li className="details__sidebar-item">
+                                        Как купить выкройку
+                                        <DetailsVectorSidbar /></li>
+                                </ul></li>
+
+                            <li className="details__sidmaster-title">
+
+                                <ul className="details__sidebar-list-kategories">
+                                    Категории
+                                    {map(arrClothes, (kategories, index) => (
+                                        <DetailsKategories kategories={kategories} key={index} />))}
+                                </ul></li>
+                            <p className="details__text">Платье из тонкого шитья на подкладке из батиста.</p>
+
+                            <li className="details__sidmaster-price">
+                                Цена: 150 руб.</li>
+
+                            <li className="details__sidebar-item">
+                                <button className="details__sidmaster-shopping">Купить <Shoppings />
+                                </button><Favourites /></li>
+                        </ul>
+                    </div>
+                </>) : (<>
+                    <DetailsModel
+                        cardValuePictures={cardValuePictures}
+                    />
+
+                    {/* Сайт-бар */}
+                    <div className="details__sidebar">
+                        <ul className="details__sidmaster">
+                            {cardValue.map((kategories, index) => (
+                                <li key={index} className="details__sidmaster-item details__sidebar-number">
+                                    Выкройка №{kategories.number}</li>))}
+
+
+                            <li className="details__sidmaster-item">
+                                <ul className="details__sidebar-list">
+                                    <li className="details__sidebar-item">
                                         Расход материала
-                                        <DetailsVectorSidbar onClick={handleOpen}/></li>
-                                    )}
+                                        <DetailsVectorSidbar /></li>
+                                    <li className="details__sidebar-item">
+                                        Рекомендации по&nbsp;материалам
+                                        <DetailsVectorSidbar /></li>
+                                    <li className="details__sidebar-item">
+                                        Как купить выкройку
+                                        <DetailsVectorSidbar /></li>
+                                </ul></li>
+
+                            <li className="details__sidmaster-title">
+
+                                <ul className="details__sidebar-list-kategories">
+                                    Категории
+                                    {map(arrClothes, (kategories, index) => (
+                                        <DetailsKategories kategories={kategories} key={index} />))}
+                                </ul></li>
+
+                            <li className="details__sidmaster-price">
+                                Цена: 150 руб.</li>
 
                             <li className="details__sidebar-item">
-                                Рекомендации по материалам
-                                <DetailsVectorSidbar /></li>
-                            <li className="details__sidebar-item">
-                                Как купить выкройку
-                                <DetailsVectorSidbar /></li>
-                        </ul></li>
-
-                    <li className="details__sidmaster-title">
-
-                        <ul className="details__sidebar-list-kategories">
-                            Категории
-                            {map(arrClothes, (kategories, index) => (
-                                <DetailsKategories kategories={kategories} key={index} />))}
-                        </ul></li>
-                    <p className="details__text">Платье из тонкого шитья на подкладке из батиста.</p>
-
-                    <li className="details__sidmaster-price">
-                        Цена: 150 руб.</li>
-
-                    <li className="details__sidebar-item">
-                        <button className="details__sidmaster-shopping">Купить <Shoppings />
-                        </button><Favourites /></li>
-                </ul>
-            </div>
-        </>) : (<>
-            <DetailsModel
-                cardValuePictures={cardValuePictures}
-            />
-
-            {/* Сайт-бар */}
-            <div className="details__sidebar">
-                <ul className="details__sidmaster">
-                    {cardValue.map((kategories, index) => (
-                        <li key={index} className="details__sidmaster-item details__sidebar-number">
-                            Выкройка №{kategories.number}</li>))}
-
-
-                    <li className="details__sidmaster-item">
-                        <ul className="details__sidebar-list">
-                            <li className="details__sidebar-item">
-                                Расход материала
-                                <DetailsVectorSidbar /></li>
-                            <li className="details__sidebar-item">
-                                Рекомендации по&nbsp;материалам
-                                <DetailsVectorSidbar /></li>
-                            <li className="details__sidebar-item">
-                                Как купить выкройку
-                                <DetailsVectorSidbar /></li>
-                        </ul></li>
-
-                    <li className="details__sidmaster-title">
-
-                        <ul className="details__sidebar-list-kategories">
-                            Категории
-                            {map(arrClothes, (kategories, index) => (
-                                <DetailsKategories kategories={kategories} key={index} />))}
-                        </ul></li>
-
-                    <li className="details__sidmaster-price">
-                        Цена: 150 руб.</li>
-
-                    <li className="details__sidebar-item">
-                        <button className="details__sidmaster-shopping">Купить <Shoppings />
-                        </button><Favourites /></li>
-                </ul>
-            </div>
-        </>)
-}
+                                <button className="details__sidmaster-shopping">Купить <Shoppings />
+                                </button><Favourites /></li>
+                        </ul>
+                    </div>
+                </>)
+                }
             </div >
 
         </>
